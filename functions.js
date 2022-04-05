81c15a19672c99a2977838453cdc5fe5b72a7944
Daphne Castro <dc4768@nyu.edu>
2022-04-05 00:38:35 -0400

    fit

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
 var numInput = document.getElementById("half-button").value;
+document.getElementById("half-button").addEventListener("click", displayNum);
+function displayNum() {
+  alert(numInput / 2);
+}
 
 
-document.getElementById("half-button").addEventListener("click", displayDate);
-
-function displayDate() {
-alert(numInput / 2);
+var nameInput = document.getElementById("fortune-button").value;
+document.getElementById("fortune-button").addEventListener("click", displayFortune);
+function displayFortune() {
+  alert(nameInput + "will have a sweet day!");
 }
