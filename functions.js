ff12845fcf3c5cce958a1e7bc075d5dfb3e2a37a
Daphne Castro <dc4768@nyu.edu>
2022-04-05 00:28:34 -0400

    hit

---
+++ functions.js
@@ -0,0 +1,8 @@
+var numInput = document.getElementById("half-button").value;
+
+
+document.getElementById("half-button").addEventListener("click", displayDate);
+
+function displayDate() {
+alert(numInput / 2);
+}
