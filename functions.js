661a737d5e85e8537f07b6b828d4d54dd4a2509f
Daphne Castro <dc4768@nyu.edu>
2022-04-05 01:06:07 -0400

    help

--- functions.js
+++ functions.js
@@ -1,12 +1,31 @@
-var numInput = document.getElementById("half-button").value;
-document.getElementById("half-button").addEventListener("click", displayNum);
-function displayNum() {
-  alert(numInput / 2);
+var numButton;
+var numInput;
+var fortuneButton;
+var fortuneInput;
+var fortuneOutput;
+var text;
+
+document.addEventListener("DOMContentLoaded", function(){
+  numButton = document.getElementById("half-button");
+  numInput = document.getElementById("half-input");
+  fortuneButton = document.getElementById("fortune-button");
+  fortuneInput = document.getElementById("fortune-input");
+  fortuneOutput = document.getElementById("fortune-output");
+  text = document.getElementById("text");
 }
 
+numButton.addEventListener("click", displayNum(){
+  var initalNum = numInput.value();
+  displayNum(initalNum);
+}
+function displayNum(num) {
+  newNum = num / 2;
+  alert(newNum);
+}
 
+/*
 var nameInput = document.getElementById("fortune-button").value;
 document.getElementById("fortune-button").addEventListener("click", displayFortune);
 function displayFortune() {
   alert(nameInput + "will have a sweet day!");
-}
+}*/
